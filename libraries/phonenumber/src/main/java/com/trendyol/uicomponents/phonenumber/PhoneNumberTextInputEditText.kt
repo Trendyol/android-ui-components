a31c640fd70d3e73bb913fea038d3db25df011c5
Mert Nevzat Yüksel <mertnevzatyuksel@gmail.com>
2019-12-13 10:44:34 +0300

    Rename module and package name to phonenumber Set version to 1.0.0

--- libraries/phonenumber/src/main/java/com/trendyol/uicomponents/phonenumber/PhoneNumberTextInputEditText.kt
+++ libraries/phonenumber/src/main/java/com/trendyol/uicomponents/phonenumber/PhoneNumberTextInputEditText.kt
@@ -1,4 +1,4 @@
-package com.trendyol.uicomponents.phonenumbertextinputedittext
+package com.trendyol.uicomponents.phonenumber
 
 import android.content.ClipData
 import android.content.ClipboardManager
