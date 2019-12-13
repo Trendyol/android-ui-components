9c075da337e26a4c6e6e5d8fa3a7a4de56585dc7
Mert Nevzat Yüksel <mertnevzatyuksel@gmail.com>
2019-12-13 10:12:21 +0300

    Add PhoneNumberTextInputEditText as a library

--- sample/src/main/java/com/trendyol/uicomponents/MainActivity.kt
+++ sample/src/main/java/com/trendyol/uicomponents/MainActivity.kt
@@ -17,5 +17,8 @@ class MainActivity : AppCompatActivity() {
         button_dialogs.setOnClickListener {
             startActivity(Intent(this, DialogsActivity::class.java))
         }
+        button_phone_number.setOnClickListener {
+            startActivity(Intent(this, PhoneNumberActivity::class.java))
+        }
     }
 }
