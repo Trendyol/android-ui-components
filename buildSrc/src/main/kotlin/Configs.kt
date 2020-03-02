a0773c0f106d150b6cc7170d02fee90dc97e13c9
Bilgehan Kalkan <bilgehankalkan68@gmail.com>
2020-03-02 17:26:34 +0300

    Create ComponentVersions. Update Configs. Update coreKtx version and add other libraries into Dependencies. Create Plugins. Move each component's gradle to kts. Update gradle version to 6.2. Update settings.gradle.

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -3,7 +3,8 @@ object Configs {
     const val compileSdkVersion = 29
     const val minSdkVersion = 17
     const val targetSdkVersion = 29
-    const val group = "com.trendyol.android-core"
-
     const val buildToolsVersion = "29.0.3"
+
+    const val applicationId = "com.trendyol.uicomponents"
+    const val group = "com.trendyol.ui-components"
 }
