87ed683e25f3133d19f3a6e630e1c3922212c4c9
Bilgehan Kalkan <bilgehankalkan68@gmail.com>
2020-03-02 16:24:24 +0300

    Create buildSrc and create Dependencies, Configs and Components.

---
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -0,0 +1,9 @@
+object Configs {
+
+    const val compileSdkVersion = 29
+    const val minSdkVersion = 17
+    const val targetSdkVersion = 29
+    const val group = "com.trendyol.android-core"
+
+    const val buildToolsVersion = "29.0.3"
+}
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,10 @@
+object Dependencies {
+
+    const val kotlinJDK = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.61"
+
+    const val coreKtx = "androidx.core:core-ktx:1.1.0"
+    const val appCompat = "androidx.appcompat:appcompat:1.1.0"
+    const val material = "com.google.android.material:material:1.0.0"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+    const val androidXMaterial = "com.google.android.material:material:1.1.0"
+}
