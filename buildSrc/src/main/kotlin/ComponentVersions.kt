f435c9d2a5867760e82246de2173e7213928e310
Mert <mertnevzatyuksel@gmail.com>
2020-05-28 10:15:54 +0300

    Update phoneNumber version to 1.0.2

--- buildSrc/src/main/kotlin/ComponentVersions.kt
+++ buildSrc/src/main/kotlin/ComponentVersions.kt
@@ -3,7 +3,7 @@ object ComponentVersions {
     const val toolbarVersion = "1.0.1"
     const val suggestionInputViewVersion = "1.0.12"
     const val ratingBarVersion = "1.0.2"
-    const val phoneNumberVersion = "1.0.1"
+    const val phoneNumberVersion = "1.0.2"
     const val imageSliderVersion = "1.0.4"
     const val dialogsVersion = "1.0.7"
     const val cardInputViewVersion = "1.0.5"
