73e3c2f7a77d9026185f07175377e761f15bd964
Mert Nevzat Yüksel <mertnevzatyuksel@gmail.com>
2020-07-17 14:01:39 +0300

    - Remove duplicate material library from Dependencies.kt - Update material library to 1.1.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,9 +4,8 @@ object Dependencies {
 
     const val coreKtx = "androidx.core:core-ktx:1.1.0"
     const val appCompat = "androidx.appcompat:appcompat:1.1.0"
-    const val material = "com.google.android.material:material:1.0.0"
+    const val material = "com.google.android.material:material:1.1.0"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
-    const val androidXMaterial = "com.google.android.material:material:1.0.0"
     const val recyclerView = "androidx.recyclerview:recyclerview:1.1.0"
     const val circleIndicator = "me.relex:circleindicator:2.1.4"
     const val glide = "com.github.bumptech.glide:glide:4.10.0"
