faf7ea8d9dc6abeb9302df5854bc128c3e62912d
bilgehan.kalkan <%&PrhD6U>
2021-02-03 16:26:30 +0300

    Update circleIndicator library source. Update image-slider version to 1.0.8.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -7,7 +7,7 @@ object Dependencies {
     const val material = "com.google.android.material:material:1.1.0"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
     const val recyclerView = "androidx.recyclerview:recyclerview:1.1.0"
-    const val circleIndicator = "me.relex:circleindicator:2.1.4"
+    const val circleIndicator = "com.github.MertNYuksel:CircleIndicator::2a2e973374"
     const val glide = "com.github.bumptech.glide:glide:4.10.0"
     const val glideCompiler = "com.github.bumptech.glide:compiler:4.10.0"
     const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:2.1.0"
