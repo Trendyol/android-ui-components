29571e2f319f15054373ac7946e887384515a45c
erol.aksoy <erol.aksoy@trendyol.com>
2022-06-28 09:07:39 +0300

    Increase dialogs version

--- buildSrc/src/main/kotlin/ComponentVersions.kt
+++ buildSrc/src/main/kotlin/ComponentVersions.kt
@@ -5,7 +5,7 @@ object ComponentVersions {
     const val ratingBarVersion = "1.0.2"
     const val imageSliderVersion = "1.0.8"
     const val phoneNumberVersion = "1.0.2"
-    const val dialogsVersion = "1.3.1"
+    const val dialogsVersion = "1.3.2"
     const val cardInputViewVersion = "1.2.1"
     const val quantityPickerViewVersion = "1.2.5"
     const val timelineViewVersion = "1.0.0"
