f80124bc3ac2003ebf9243f4341f9f0fad760ee8
turkergoksu <turkergoksu@gmail.com>
2022-07-28 09:13:18 +0300

    Revert some debounced click listeners to not disrupt as-is behavior

--- buildSrc/src/main/kotlin/ComponentVersions.kt
+++ buildSrc/src/main/kotlin/ComponentVersions.kt
@@ -1,6 +1,6 @@
 object ComponentVersions {
 
-    const val toolbarVersion = "2.0.7"
+    const val toolbarVersion = "2.0.8"
     const val suggestionInputViewVersion = "1.3.0"
     const val ratingBarVersion = "1.0.2"
     const val imageSliderVersion = "1.0.8"
