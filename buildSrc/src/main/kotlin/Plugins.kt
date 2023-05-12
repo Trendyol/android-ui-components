7f20b55e8603dfb693e1d285a1e00c68b6fbc41e
erol.aksoy <erol.aksoy@trendyol.com>
2023-05-12 13:29:51 +0300

    Create sample-compose app

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -1,7 +1,7 @@
 object Plugins {
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:7.3.1"
-    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20"
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10"
 
     const val androidApplication = "com.android.application"
     const val androidLibrary = "com.android.library"
