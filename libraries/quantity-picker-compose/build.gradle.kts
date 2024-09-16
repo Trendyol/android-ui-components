8322c7420e6313f89477221b842ffc25f02709ad
burak.ozturk1 <burak.ozturk1@trendyol.com>
2024-09-16 22:55:04 +0300

    Create quantityPickerCompose

---
+++ libraries/quantity-picker-compose/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id(Plugins.androidLibrary)
+    id(Plugins.kotlinAndroid)
+    id(Plugins.mavenPublish)
+    id(Plugins.compose) version "2.0.0"
+}
+
+kotlin.jvmToolchain(17)
+
+android {
+    namespace = "com.trendyol.quantitypickercompose"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = Configs.minSdkVersion
+        vectorDrawables.useSupportLibrary = true
+    }
+
+    buildTypes {
+        getByName<com.android.build.gradle.internal.dsl.BuildType>("release") {
+            isMinifyEnabled = false
+            setProguardFiles(
+                mutableListOf(
+                    getDefaultProguardFile("proguard-android.txt"),
+                    "proguard-rules.pro"
+                )
+            )
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = Dependencies.kotlinCompilerExtensionVersion
+    }
+}
+
+dependencies {
+
+    val composeBom = platform(Dependencies.composeBom)
+    implementation(composeBom)
+    implementation(Dependencies.composeMaterial)
+    implementation(Dependencies.composeUiTooling)
+    implementation(Dependencies.composeCoil)
+    implementation(Dependencies.composeRuntime)
+    implementation(Dependencies.composeConstraintLayout)
+}
